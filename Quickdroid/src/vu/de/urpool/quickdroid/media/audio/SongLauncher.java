ad6bec4e96c370dac168738d031f19390fbd1b31
daniel.himmelein <daniel.himmelein@gmail.com>
2010-05-04 20:23:12 +0000

    merged from HEAD

--- Quickdroid/src/vu/de/urpool/quickdroid/media/audio/SongLauncher.java
+++ Quickdroid/src/vu/de/urpool/quickdroid/media/audio/SongLauncher.java
@@ -152,7 +152,7 @@ public class SongLauncher extends Launcher {
     	if (launchable instanceof SongLaunchable) {
     		Intent intent = new Intent(Intent.ACTION_VIEW,
 				ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, launchable.getId()));
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
 			List<ResolveInfo> list = mContext.getPackageManager().queryIntentActivities(intent, 
 				PackageManager.MATCH_DEFAULT_ONLY);
 			if(list.size() > 0) {

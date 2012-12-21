2d73103fcce773d9f45b0b706b150bc2b7efa702
daniel.himmelein@googlemail.com <daniel.himmelein@googlemail.com@dc49121e-7c84-11de-87c4-3d7c82793bd4>
2012-12-21 20:52:21 +0000

    Fixed READ_EXTERNAL_STORAGE exception caused by reading from com.android.providers.media.MediaProvider

--- trunk/Quickdroid/src/vu/de/urpool/quickdroid/Quickdroid.java
+++ trunk/Quickdroid/src/vu/de/urpool/quickdroid/Quickdroid.java
@@ -637,7 +637,7 @@ public class Quickdroid extends ListActivity implements OnGesturePerformedListen
 	
 	private void checkSettings() {
 		int versionCode = mSettings.getInt("versionCode", 7);
-		if (versionCode < 45) {
+		if (versionCode < 46) {
 			SharedPreferences.Editor editor = mSettings.edit();
 			if (versionCode < 8) {
 				editor.putInt("versionCode", 8);
@@ -674,7 +674,7 @@ public class Quickdroid extends ListActivity implements OnGesturePerformedListen
 				appsEditor.putInt("syncState", AppProvider.OUT_OF_SYNC);
 				appsEditor.commit();
 			}
-			editor.putInt("versionCode", 45);
+			editor.putInt("versionCode", 46);
 			editor.commit();
 		}
 	}
